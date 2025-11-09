0215475e60be503d81e9959cffa04393f93d0da9
Hasnat4763 <abulhasnat5737@gmail.com>
2025-11-09 19:53:06 +0600

    Adding comments
    Added some comments for code so it is more readable

--- scripts/main_game.gd
+++ scripts/main_game.gd
@@ -1,24 +1,24 @@
 extends Node2D
 
 var Dice_Run = 0
-var player_or_ai = [true, true, true, true]
-var turn = 0
-var Dice_Face = 0
-var PlayerPosition : Array = [0, 0, 0, 0]
-var Player_Path : Array = []
+var player_or_ai = [true, true, true, true] # "true" means Ai, "False" is human
+var turn = 0 # turn is used for switching between players
+var Dice_Face = 0 # Dice Face is used to show the top of Dice
+var PlayerPosition : Array = [0, 0, 0, 0] # It stores the player's position in game
+var Player_Path : Array = [] # This array holds the player sprite path
 var is_running : bool = false
-var one_or_two_map : bool = true
+var one_or_two_map : bool = true # This is used to switch between boards
 var Last_Won : String
-var time_running : float = 0
-var last_updated = 0
-var file_path = "res://save_score.data"
+var time_running : float = 0 # It counts how long the game is running
+var last_updated = 0 # it helps display the time
+var file_path = "res://save_score.data" # save file path
 
 var Players = {
 	0: "Yellow",
 	1: "Blue",
 	2: "Red",
 	3: "Green"
-}
+} # It keeps the Player Colors
 
 var Dice_Position = {
 	0: Vector2(122, 939),
@@ -26,25 +26,8 @@ var Dice_Position = {
 	2: Vector2(455, 52),
 	3: Vector2(452, 937)
 }
+# This is used to save the location where the Dice needs to move
 
-var two_Ladder_Location = {
-	4: 23,
-	8: 28,
-	14: 46,
-	30: 49,
-	40: 58,
-	56: 76,
-	70: 72,
-	80: 98,
-	89: 93
-}
-var two_Snake_Location = {
-	99: 67,
-	91: 1,
-	79: 7,
-	60: 24,
-	55: 10
-}
 
 var one_Ladder_Location = {
 	2: 18,
@@ -58,6 +41,7 @@ var one_Ladder_Location = {
 	79: 98,
 	89: 91
 }
+# This keeps the location and destination of ladders for the 1st board
 
 var one_Snake_Location = {
 	99: 81,
@@ -71,6 +55,28 @@ var one_Snake_Location = {
 	38: 17,
 	27: 5
 }
+#This keeps the location and destination of the snakes in the 1st board
+
+var two_Ladder_Location = {
+	4: 23,
+	8: 28,
+	14: 46,
+	30: 49,
+	40: 58,
+	56: 76,
+	70: 72,
+	80: 98,
+	89: 93
+}
+# This keeps the location and destination of the ladders in the 2nd board
+var two_Snake_Location = {
+	99: 67,
+	91: 1,
+	79: 7,
+	60: 24,
+	55: 10
+}
+#This keeps the location and the destination of the snakes in the 2nd board
 
 func _ready() -> void:
 	load_game()
@@ -82,7 +88,6 @@ func _ready() -> void:
 	$StartScreen.fourplayer.connect(four_player)
 	$StartScreen.change_map.connect(change_map)
 	$endScreen.startScreen.connect(go_back_to_startScreen)
-	
 	$Dice_Animation.hide()
 	$Dice_Drawed.hide()
 	$StartScreen/last_winner.text = str(Last_Won) + " won last time"
@@ -90,20 +95,20 @@ func _ready() -> void:
 	if one_or_two_map:
 		$oneboard/Path.hide()
 		$oneboard/Playing_Board.hide()
-		Player_Path = [$oneboard/Path/Yellow_Player, $oneboard/Path/Blue_Player, $oneboard/Path/Red_Player, $oneboard/Path/Green_Player]
-		$twoboard.hide()
+		Player_Path = [$oneboard/Path/Yellow_Player, $oneboard/Path/Blue_Player, $oneboard/Path/Red_Player, $oneboard/Path/Green_Player] # Sets the Player path for the 1st board
+		$twoboard.hide()  # Hides the 2nd board so it doesnt interfere
 	else:
 		$twoboard/Path.hide()
 		$twoboard/Playing_Board.hide()
-		$oneboard.hide()
-		Player_Path = [$twoboard/Path/Yellow_Player, $twoboard/Path/Blue_Player, $twoboard/Path/Red_Player, $twoboard/Path/Green_Player]
+		$oneboard.hide() # Hides the 1st board so it doesnt interfere
+		Player_Path = [$twoboard/Path/Yellow_Player, $twoboard/Path/Blue_Player, $twoboard/Path/Red_Player, $twoboard/Path/Green_Player] # Sets the player path for the 2nd board
 	$endScreen.hide()
 
 func change_map():
-	one_or_two_map = false
+	one_or_two_map = false # Switches the board
 
 func start_game() -> void:
-	is_running = true
+	is_running = true #Sets the game as running
 	$Dice_Drawed.show()
 	if one_or_two_map:
 		$oneboard.show()
@@ -116,33 +121,34 @@ func start_game() -> void:
 	$StartScreen.hide()
 	Dice_Mover(turn)
 	
-	if not is_running:
+	if not is_running: # Checks if the game is running to prevent starting
 		return
-	if player_or_ai[turn]:
+	if player_or_ai[turn]: # Checks if the turn is of a player or if it is an AI
 		await get_tree().create_timer(1.0).timeout
 		ai_turn()
 	else:
 		Dice_Run = 1
 
 func _on_dice_drawed_pressed() -> void:
-	if Dice_Run == 0:
+	if Dice_Run == 0: # Checks if It is allowed to roll yet
 		return
-	if $StartScreen.visible:
+	if $StartScreen.visible: # Checks if the game is still on startscreen
 		return
 	if not is_running:
 		return
-	$Dice_Roll.play()
+	$Dice_Roll.play() # Plays the Dice Roll Sound
 	$Dice_Drawed.hide()
 	$Dice_Animation.show()
-	$Dice_Animation.play("dice_draw")
-	Dice_Run = 0
-func ai_turn() -> void:
+	$Dice_Animation.play("dice_draw") # Plays the Dice rolling animation
+	Dice_Run = 0 # Sets so that the Dice can't be rolled untill the value is 1
+	
+func ai_turn() -> void: # this function is the AI
 	$Dice_Roll.play()
 	$Dice_Drawed.hide()
 	$Dice_Animation.show()
 	$Dice_Animation.play("dice_draw")
 
-func _on_dice_animation_finished() -> void:
+func _on_dice_animation_finished() -> void: # This function picks a random integer from 0 to 5 and uses that as the dice number
 	if not is_running:
 		return
 	randomize()
@@ -160,22 +166,23 @@ func _on_dice_animation_finished() -> void:
 				Path_Point = $oneboard/Path.get_curve().get_point_position(0)
 			else:
 				Path_Point = $twoboard/Path.get_curve().get_point_position(0)
-			Player_Path[turn].position = set_offset_for_pawns(Path_Point, turn)
+			Player_Path[turn].position = set_offset_for_pawns(Path_Point, turn) # sets an offset so multiple pawns can be on single square
 	else:
 		await _move_player(turn, dice_value)
+		
 	await get_tree().create_timer(0.5).timeout
-	if PlayerPosition[turn] >= 100:
+	if PlayerPosition[turn] >= 100: # This checks if the condition is fullfilled
 		endgame()
-	turn = (turn + 1) % 4
-	Dice_Mover(turn)
+	turn = (turn + 1) % 4 # the remainder is used as the turn mover
+	Dice_Mover(turn) # This moves the Player to the Destination
 	
 	if player_or_ai[turn]:
 		await get_tree().create_timer(1.0).timeout
 		ai_turn()
 	else:
-		Dice_Run = 1
+		Dice_Run = 1 # Sets it so the Dice can be rolled again
 	
-	$Dice_Drawed/Dice_Drawed.show()
+	$Dice_Drawed/Dice_Drawed.show() # Shows the Dice rolling button so it can be pressed
 
 func _move_player(player_index: int, steps: int) -> void:
 	if not is_running:
@@ -188,7 +195,7 @@ func _move_player(player_index: int, steps: int) -> void:
 	else:
 		curve = $twoboard/Path.get_curve()
 	
-	var tween = create_tween()
+	var tween = create_tween() # This animates the player movement
 
 	for i in range(steps):
 		if PlayerPosition[player_index] >= 100:
@@ -202,17 +209,18 @@ func _move_player(player_index: int, steps: int) -> void:
 	await tween.finished
 
 	var current = PlayerPosition[player_index]
+	
 	if one_or_two_map:
 		if current in one_Ladder_Location:
-			PlayerPosition[player_index] = one_Ladder_Location[current]
+			PlayerPosition[player_index] = one_Ladder_Location[current] # This Checks and sets the player location according to the ladders in the 1st board
 		elif current in one_Snake_Location:
-			PlayerPosition[player_index] = one_Snake_Location[current]
+			PlayerPosition[player_index] = one_Snake_Location[current] # This checks and sets the player location according to the snakes in the 1st board
 			$Snake.play()
 	else:
 		if current in two_Ladder_Location:
-			PlayerPosition[player_index] = two_Ladder_Location[current]
+			PlayerPosition[player_index] = two_Ladder_Location[current] # This sets the player location according to the ladders on the 2nd map
 		elif current in two_Snake_Location:
-			PlayerPosition[player_index] = two_Snake_Location[current]
+			PlayerPosition[player_index] = two_Snake_Location[current] # This sets the player location according to the snakes on the 2nd map
 			$Snake.play()
 	if one_or_two_map:
 		if current in one_Ladder_Location or current in one_Snake_Location:
@@ -228,7 +236,9 @@ func _move_player(player_index: int, steps: int) -> void:
 			var jump_tween = create_tween()
 			jump_tween.tween_property(player_node, "position", offset_final, 0.5).set_trans(Tween.TRANS_BOUNCE).set_ease(Tween.EASE_OUT)
 			await jump_tween.finished
-func Dice_Mover(player_index: int) -> void:
+	# These start the animation of the player for the snakes and ladders
+			
+func Dice_Mover(player_index: int) -> void: # This function moves the Dice to every player's location
 	if not is_running:
 		return
 	
